8e234ef2ec688f69b95108f98ef3644be9297d69
kishor <74954474+kishorjk@users.noreply.github.com>
2021-07-04 18:55:14 +0530

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,7 +6,7 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "kishor",
   middleName: "",
   lastName: "Shoaib",
   message: " Passionate about changing the world with technology. ",
